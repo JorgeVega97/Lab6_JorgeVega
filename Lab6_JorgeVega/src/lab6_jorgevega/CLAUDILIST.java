7141b1a94f80e668c74cd251b367eee4000f7344
JorgeVega97 <jorgeenriquevegamaradiaga6@gmail.com>
2020-11-27 16:53:33 -0600

    creacion de clases y form

--- Lab6_JorgeVega/src/lab6_jorgevega/CLAUDILIST.java
+++ Lab6_JorgeVega/src/lab6_jorgevega/CLAUDILIST.java
@@ -9,13 +9,6 @@ package lab6_jorgevega;
  *
  * @author TECNOVA
  */
-public class Lab6_JorgeVega {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class CLAUDILIST {
     
 }
