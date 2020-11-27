0a84ff61fd6ee46225411ece05d04572fd870470
JorgeVega97 <jorgeenriquevegamaradiaga6@gmail.com>
2020-11-27 17:01:51 -0600

    Clases claudilist y programas

--- Lab6_JorgeVega/src/lab6_jorgevega/PROGRAMA.java
+++ Lab6_JorgeVega/src/lab6_jorgevega/PROGRAMA.java
@@ -1,14 +1,66 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package lab6_jorgevega;
 
 /**
- *
- * @author TECNOVA
+JorgeVega
  */
 public class PROGRAMA {
+    private String NOMBRE;
+    private int PUNTUACION;
+    private int LANZAMIENTO;
+    private String TIPO;
+    private String GENERO;
+
+    public PROGRAMA(String NOMBRE, int PUNTUACION, int LANZAMIENTO, String TIPO, String GENERO) {
+        this.NOMBRE = NOMBRE;
+        this.PUNTUACION = PUNTUACION;
+        this.LANZAMIENTO = LANZAMIENTO;
+        this.TIPO = TIPO;
+        this.GENERO = GENERO;
+    }
+
+    public String getGENERO() {
+        return GENERO;
+    }
+
+    public void setGENERO(String GENERO) {
+        this.GENERO = GENERO;
+    }
+
+    public String getNOMBRE() {
+        return NOMBRE;
+    }
+
+    public void setNOMBRE(String NOMBRE) {
+        this.NOMBRE = NOMBRE;
+    }
+
+    public int getPUNTUACION() {
+        return PUNTUACION;
+    }
+
+    public void setPUNTUACION(int PUNTUACION) {
+        this.PUNTUACION = PUNTUACION;
+    }
+
+    public int getLANZAMIENTO() {
+        return LANZAMIENTO;
+    }
+
+    public void setLANZAMIENTO(int LANZAMIENTO) {
+        this.LANZAMIENTO = LANZAMIENTO;
+    }
+
+    public String getTIPO() {
+        return TIPO;
+    }
+
+    public void setTIPO(String TIPO) {
+        this.TIPO = TIPO;
+    }
+
+    @Override
+    public String toString() {
+        return NOMBRE;
+    }
     
 }
